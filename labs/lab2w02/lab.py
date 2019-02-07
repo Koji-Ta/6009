0cd898d6d8e5fb46760a006cfedcd0095a05bae1
Dmytro Shaban <10shaban72@gmail.com>
2019-02-07 11:46:28 +0200

    Lab 2. Answer questions to did_x_and_y_act_together() function

--- labs/lab2w02/lab.py
+++ labs/lab2w02/lab.py
@@ -11,6 +11,11 @@ def did_x_and_y_act_together(data, actor_id_1, actor_id_2):
     return False
 
 
+def get_actor_id(data, name):
+    """Return actor id in data by his/her name"""
+    return data[name]
+
+
 def get_actors_with_bacon_number(data, n):
     raise NotImplementedError("Implement me!")
 
@@ -27,4 +32,22 @@ if __name__ == '__main__':
     # additional code here will be run only when lab.py is invoked directly
     # (not when imported from test.py), so this is a good place to put code
     # used, for example, to generate the results for the online questions.
-    pass
+    with open('resources/names.json') as f:
+        names = json.load(f)
+
+    print('Did Barbara Flynn and Kurtwood Smith acted together?',
+          did_x_and_y_act_together(smalldb,
+                                   get_actor_id(names, 'Barbara Flynn'),
+                                   get_actor_id(names, 'Kurtwood Smith')))
+    print('Did David Stevens and Folke Lind acted together?',
+          did_x_and_y_act_together(smalldb,
+                                   get_actor_id(names, 'David Stevens'),
+                                   get_actor_id(names, 'Folke Lind')))
+    print('Did Dominique Reymond and Eduardo Yanez acted together?',
+          did_x_and_y_act_together(smalldb,
+                                   get_actor_id(names, 'Dominique Reymond'),
+                                   get_actor_id(names, 'Eduardo Yanez')))
+    print('Did Chris Hogan and Charles Berling acted together?',
+          did_x_and_y_act_together(smalldb,
+                                   get_actor_id(names, 'Chris Hogan'),
+                                   get_actor_id(names, 'Charles Berling')))
